f872a43f415c618b5b488c59d202a9aa47f697bf
zx648383079 <648383079@qq.com>
2016-11-06 13:12:38 +0800

    增加递归创建文件夹

--- ZoDream.Helper/ZoDream.Helper/Local/FileHelper.cs
+++ ZoDream.Helper/ZoDream.Helper/Local/FileHelper.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ZoDream.Helper.Local
 {
@@ -68,6 +69,67 @@ namespace ZoDream.Helper.Local
                 sb.Append(item.ToString("X2"));
             }
             return sb.ToString();
+        }
+
+        /// <summary>
+        /// Creates a relative path from one file or folder to another.
+        /// </summary>
+        /// <param name="fromPath">Contains the directory that defines the start of the relative path.</param>
+        /// <param name="toPath">Contains the path that defines the endpoint of the relative path.</param>
+        /// <returns>The relative path from the start directory to the end path.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static string MakeRelativePath(string fromPath, string toPath)
+        {
+            if (string.IsNullOrEmpty(fromPath))
+            {
+                return toPath;
+            };
+            if (string.IsNullOrEmpty(toPath))
+            {
+                return "";
+            };
+
+            var fromUri = new Uri(fromPath);
+            var toUri = new Uri(toPath);
+
+            var relativeUri = fromUri.MakeRelativeUri(toUri);
+            var relativePath = Uri.UnescapeDataString(relativeUri.ToString());
+
+            return relativePath.Replace('/', Path.DirectorySeparatorChar);
+        }
+
+        public static string GetRelativePaths(string path, string current)
+        {
+            var a = current.ToLower();
+            var b = path.ToLower();
+            var i = 0;
+            for (; i < Math.Min(a.Length, b.Length); i++)
+            {
+                if (a[i] != b[i]) break;
+            }
+            var cur = Regex.Replace(a.Substring(i - 1), @"\\?[a-zA-Z]+:?", @"..\");
+            return (cur + path.Substring(i)).Replace(@"\\", @"\");
+        }
+
+        public void CreateDirectory(string filefullpath)
+        {
+            if (File.Exists(filefullpath))
+            {
+                return;
+            }
+             //判断路径中的文件夹是否存在
+            var dirpath = filefullpath.Substring(0, filefullpath.LastIndexOf('\\'));
+            var pathes = dirpath.Split('\\');
+            if (pathes.Length <= 1) return;
+            var path = pathes[0];
+            for (var i = 1; i < pathes.Length; i++)
+            {
+                path += "\\" + pathes[i];
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+            }
         }
     }
 }
